3bbad6bc4091b29530e30a7fdb7e964e5694c162
shrikantrpatil7 <118796468+shrikantrpatil7@users.noreply.github.com>
2024-01-01 17:41:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('Hello all Welcome to the AeronFly Group'); //write a response to the client
+  res.write('This Website is Hosted by Shrikant Patil'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
